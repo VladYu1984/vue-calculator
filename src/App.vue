2d86995ae5ca105850a34bb2b960bac51d809367
VladYu1984 <87527162+VladYu1984@users.noreply.github.com>
2022-09-08 03:27:14 +0300

    vue-calculator
    Simple app calculator Vue.js

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <CalculatorApp/>
+</div>
+</template>
+
+<script>
+import CalculatorApp from './components/CalculatorApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+</style>
